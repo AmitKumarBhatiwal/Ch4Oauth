513e7b710902af6532344cccc0e5e26284ad7e21
Amit kumar <amit.kumar@naskay.in>
2023-04-03 14:02:03 +0530

    Initial commit

---
+++ src/main/java/com/springbooy/Ch4Oauth/Ch4OauthApplication.java
@@ -0,0 +1,13 @@
+package com.springbooy.Ch4Oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch4OauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch4OauthApplication.class, args);
+	}
+
+}
